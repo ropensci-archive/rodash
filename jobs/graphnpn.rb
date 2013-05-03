f48d2087acb229c204ce61e54ae6ea39726ce783
schamberlain <myrmecocystus@gmail.com>
2013-05-03 10:31:05 -0700

    changed graphnpn job to 20s from 60s

--- jobs/graphnpn.rb
+++ jobs/graphnpn.rb
@@ -7,7 +7,7 @@ points = []
 end
 last_x = points.last[:x]
 
-SCHEDULER.every '60s', :first_in => 0 do |job|
+SCHEDULER.every '20s', :first_in => 0 do |job|
   result = HTTParty.get("https://www.usanpn.org/npn_portal/observations/getAllObservationsForSpecies.json?species_id[0]=52&start_date=2011-01-01&end_date=2011-02-01")
 
 	if result['station_list'][0]['station_id'] == "4881"
